480944c21b25e33d0276e691edd31a9aacdb0731
Tharak Ram <tharakarame@gmail.com>
2026-04-28 12:13:46 +0530

    Implement escape sequence examples in escape_sequence.py
    Add examples of various escape sequences in Python.

---
+++ escape_sequence.py
@@ -0,0 +1,14 @@
+#\n  next line
+print("Hello everyone.\nHow are you?")
+
+#\t
+print("Ram\t20")
+
+#\\
+print("yes\\no")
+
+#\'
+print('this is Ram\'s class')
+
+#\"
+print("He says,\"we are learing python\"")
